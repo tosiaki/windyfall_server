90fc2f56a9be3f574a0373a31d60bb16058bc4c3
author <example@test.com>
2020-02-01 22:06:39 -0500

    First commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from aiohttp import web
+import socketio
+
+app = web.Application()
+sio = socketio.AsyncServer(async_mode='aiohttp')
+sio.attach(app)
+
+@sio.event
+def connect(sid, environ):
+    print(f'Connection from {environ["REMOTE_ADDR"]}')
+    print(sid)
+
+if __name__ == '__main__':
+    web.run_app(app)
